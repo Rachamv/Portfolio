07a8e5c1e73ff735872cf727215e8e2163e2d908
Rachamv <rkutalian@gmail.com>
2024-01-28 17:27:49 +0100

    add a new pluging

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   const [load, upadateLoad] = useState(true);
@@ -42,6 +43,7 @@ function App() {
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
+        <SpeedInsights />
       </div>
     </Router>
   );
